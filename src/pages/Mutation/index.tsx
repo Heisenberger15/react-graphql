cf057c876b39ae4e859f09899361af002f7bc8c6
sina parsa <sinaparsania15@gmail.com>
2023-02-07 14:48:41 +0330

    feat(mutation): add simple motation for testing

---
+++ src/pages/Mutation/index.tsx
@@ -0,0 +1,26 @@
+import { gql, useMutation } from "@apollo/client";
+
+const CREATE_PRODUCT = gql`
+  mutation CreateProduct($name: String!, $quantityPerUnit: Int!) {
+    createProduct(record: { name: $name, quantityPerUnit: $quantityPerUnit }) {
+      record {
+        name
+      }
+    }
+  }
+`;
+
+export function Mutation() {
+  const [createProduct, { data, error, loading }] = useMutation(CREATE_PRODUCT, {
+    variables: {
+      name: "hotdog",
+      quantityPerUnit: 4,
+    },
+  });
+
+  return (
+    <div>
+      <button onClick={() => createProduct()}>test</button>
+    </div>
+  );
+}
